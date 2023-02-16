bb48e75f308a3f41e1dfaf7a4d72cbf728aca2aa
Jules Coquard <jcoquard@z4r6p5.42lyon.fr>
2023-02-16 17:18:23 +0100

    open close

--- so_long/main.c
+++ so_long/main.c
@@ -1,13 +1,30 @@
-#include "minilibx-linux/mlx.h"
+#include "so_long.h"
 
-int main()
+int	close_window(t_wins *win)
 {
-    void    *mlx_ptr;
-    void    *win_ptr;
+	mlx_destroy_window(win->mlx, win->win);
+	mlx_destroy_display(win->mlx);
+	free(win->mlx);
+	exit (0);
+	return (0);
+}
 
-    mlx_ptr = mlx_init();
-    win_ptr = mlx_new_window(mlx_ptr, 500, 500, "so_long");
-    mlx_loop(mlx_ptr);
-    (void) win_ptr;
-    return (0);
+int event_manager(int keycode, t_wins *win)
+{
+	printf("keycode:%d\n",keycode);
+	if(keycode == 65307)
+		close_window(win);
+	return (keycode);
+}
+
+int	main()
+{
+	t_wins	window;
+
+	window.mlx = mlx_init();
+	window.win = mlx_new_window(window.mlx, 500, 500, "so_long");
+	mlx_hook(window.win, 2, 1L<<0, event_manager, &window);
+	mlx_hook(window.win, 17, 1L<<0, close_window, &window);
+	mlx_loop(window.mlx);
+	return (0);
 }
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -1,2 +1,14 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include "minilibx-linux/mlx.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-#include "minilibx-linux/mlx.h"
+typedef struct s_wins
+{
+	void	*mlx;
+	void	*win;
+}   t_wins;
+
+#endif
